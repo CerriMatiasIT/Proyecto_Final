e56c6fbf07d6ed2f16379ae0ee9ccbdca9639031
CerriMatiasIT <cerrimatiasit@gmail.com>
2026-09-15 09:28:56 -0300

    Copiado de Repo de la profe

--- e-commerce-react-Cerri-2026/src/App.jsx
+++ e-commerce-react-Cerri-2026/src/App.jsx
@@ -1,10 +1,18 @@
-import './App.css'
-import {Header} from './components/Header/Header.jsx'
+import "./App.css";
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
+import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
+
 function App() {
   return (
     <>
-    </>  
+      <Header />
+      <main>
+        <ItemListContainer />
+      </main>
+      <Footer />
+    </>
   );
 }
 
-export default App
+export default App;
--- e-commerce-react-Cerri-2026/src/components/Footer/Footer.jsx
+++ e-commerce-react-Cerri-2026/src/components/Footer/Footer.jsx
@@ -1,1 +1,14 @@
-import "./Footer.css"
+import "./Footer.css";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <nav>
+        <ul className="nav-list">
+          <li>Whatsapp</li>
+          <li>Instagram</li>
+        </ul>
+      </nav>
+    </footer>
+  );
+};
